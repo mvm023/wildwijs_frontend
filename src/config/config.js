8784fad905415ba30c671bdaa25f6f252641d8f2
melle <70153318+mvm023@users.noreply.github.com>
2025-03-01 10:54:30 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = window.location.hostname === "localhost"
   ? "http://127.0.0.1:8000"
-  : "https://natuur-game.onrender.com";
+  : "https://wildwijs-backend.onrender.com";
 
 export default API_BASE_URL;
