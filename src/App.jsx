51a42589969f4acb44c66bf148d64c5cf8235fd7
melle <70153318+mvm023@users.noreply.github.com>
2025-03-02 11:20:32 +0100

    frontend nu mbv vite

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import QuizMode from './components/Quiz/QuizMode';
+import StudyMode from './components/Quiz/StudyMode';
+import TopBar from './components/UI/TopBar';
+import "./styles/styles.css";
+
+
+function App() {
+  // State to track the current mode
+  const [mode, setMode] = useState('quiz'); // default mode is 'quiz'
+
+  // Switch mode function
+  const switchMode = (newMode) => {
+    setMode(newMode);
+  };
+
+  return (
+    <div className="App">
+      <TopBar onSwitchMode={switchMode} currentMode={mode} />
+      <div className="content">
+        {mode === 'quiz' ? <QuizMode /> : <StudyMode />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
