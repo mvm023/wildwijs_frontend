86770416e83341075b54e603f3fd5500b776ee0d
melle <70153318+mvm023@users.noreply.github.com>
2025-05-01 22:33:17 +0200

    Visual implementation quizlayers

--- src/components/QuizSelection.jsx
+++ src/components/QuizSelection.jsx
@@ -1,25 +1,38 @@
 import React from "react";
-import API_BASE_URL from "../config/config";
 
 const QuizSelection = ({ startQuiz, quizzes }) => {
+  // Group quizzes by layer level
+  console.log(quizzes); // Check if level data is coming through
 
-    return (
-      <div id="quizSelection">
-        <h2>Selecteer een quiz</h2>
-        <div className="quiz-options">
-          {quizzes.map((quiz, index) => (
-            <div
-              key={index}
-              className="quiz-option"
-              onClick={() => startQuiz(quiz.id)}
-            >
-              <img src={quiz.image_url} alt={quiz.name} />
-              <p>{quiz.name}</p>
-            </div>
-          ))}
+  const groupedByLayer = quizzes.reduce((acc, quiz) => {
+    const layerKey = `Niveau ${quiz.layer.level}`;
+    if (!acc[layerKey]) acc[layerKey] = [];
+    acc[layerKey].push(quiz);
+    return acc;
+  }, {});
+
+  return (
+    <div id="quizSelection">
+      <h2>Selecteer een quiz</h2>
+      {Object.entries(groupedByLayer).map(([layerName, layerQuizzes]) => (
+        <div key={layerName} className="quiz-layer-group">
+          <h3>{layerName}</h3>
+          <div className="quiz-options">
+            {layerQuizzes.map((quiz) => (
+              <div
+                key={quiz.id}
+                className="quiz-option"
+                onClick={() => startQuiz(quiz.id)}
+              >
+                <img src={quiz.image_url} alt={quiz.name} />
+                <p>{quiz.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    );
-  };
+      ))}
+    </div>
+  );
+};
 
 export default QuizSelection;
