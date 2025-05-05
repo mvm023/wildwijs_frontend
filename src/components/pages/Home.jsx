7faaa5df6698e60774c2d89eebd57a59dd899c9b
melle <70153318+mvm023@users.noreply.github.com>
2025-05-05 14:14:02 +0200

    First steps in setting up homepage

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { Card, CardContent, Grid2, Typography } from '@mui/material'
+import colors from '../../theme/colors'; 
 
 const Home = () => {
   return (
-    <div>
-        Word wijs in het wild!
+    <div className='container'>
+      <Grid2 container spacing={2} style={{ paddingTop: 64 }}>
+        <Grid2 xs={12} md={6} sx={{ alignItems: 'center'}}>
+          <Typography variant='h2' sx={{ fontWeight: 500}}>
+            Wordt{' '}
+            <span style={{ fontStyle: 'italic', color: colors.accent }}>wijs</span>{' '}
+            in het{' '}
+            <span style={{ fontStyle: 'italic', color: colors.accent }}>wild!</span>
+          </Typography>
+          <Grid2 container spacing={2} sx={{ marginTop: 4 }}>
+            {[1, 2, 3].map((item, index) => (
+              <Grid2 xs={12} md={4} key={index}>
+                <Card>
+                  <img 
+                    src="https://via.placeholder.com/300x200" 
+                    alt={`Placeholder ${index + 1}`} 
+                    style={{ width: '100%', height: 'auto' }} 
+                  />
+                  <CardContent>
+                    <Typography variant="h6" align="center">Naam {index + 1}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid2>
+            ))}
+          </Grid2>
+        </Grid2>
+        <Grid2 xs={12} md={6}></Grid2>
+      </Grid2>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
